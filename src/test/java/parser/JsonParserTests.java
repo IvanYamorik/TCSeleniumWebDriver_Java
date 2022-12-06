560edb1762e40141a8c19519ddf5a6ac4e466d46
IvanYamorik <i.v.yamorik@gmail.com>
2022-12-06 11:46:30 +0300

    UnitTesting-Parser_tests2

--- src/test/java/parser/JsonParserTests.java
+++ src/test/java/parser/JsonParserTests.java
@@ -1,9 +1,12 @@
 package parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import shop.Cart;
+import shop.RealItem;
 
-import java.io.File;
+import java.io.*;
 
 public class JsonParserTests {
     @Test
@@ -11,5 +14,30 @@ public class JsonParserTests {
         Parser parser = new JsonParser();
         Cart cartFromFile = parser.readFromFile(new File("src/main/resources/eugen-cart.json"));
         assertEquals("eugen-cart", cartFromFile.getCartName());
+    }
+
+    @Test
+    void writeToFile_createsFile_ivanCart() {
+        Cart ivanCart = new Cart("ivan-cart");
+        Parser parser = new JsonParser();
+        parser.writeToFile(ivanCart);
+        File f = new File("src/main/resources/ivan-cart.json");
+        assertTrue(f.exists());
+    }
+
+    @Test
+    void writeToFile_createsItemInFile() throws IOException {
+        String expectedJson = "{\"cartName\":\"test-cart\",\"realItems\":[{\"weight\":0.0,\"name\":\"testName\",\"price\":0.0}],\"virtualItems\":[],\"total\":0.0}";
+        Cart testCart = new Cart("test-cart");
+        RealItem testItem = new RealItem();
+        testItem.setName("testName");
+        testCart.addRealItem(testItem);
+        Parser parser = new JsonParser();
+        parser.writeToFile(testCart);
+        File f = new File("src/main/resources/test-cart.json");
+        BufferedReader reader = new BufferedReader(new FileReader(f));
+        String actualJson = reader.readLine();
+        assertEquals(expectedJson, actualJson);
+
     }
 }
